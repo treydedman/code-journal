eb12f9c029559693ffcb65f1793cd192bda60021
Trey Dedman <treydedman@gmail.com>
2025-01-16 17:48:27 +0000

    completed issue #1

--- js/data.js
+++ js/data.js
@@ -7,17 +7,21 @@ const data = {
 };
 // key for localStorage
 const localStorageKey = 'code-journal-data';
+// serialize and save to localStorage
 function writeData(data) {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem(localStorageKey, dataJSON);
 }
+// load data from localStorage
 function loadData() {
   const dataJSON = localStorage.getItem(localStorageKey);
   if (dataJSON) {
     return JSON.parse(dataJSON);
   } else {
+    // if no data - initialize default data
     data.entries = [];
     data.nextEntryId = 1;
   }
 }
+// load the data
 loadData();
--- ts/data.ts
+++ ts/data.ts
@@ -1,3 +1,9 @@
+interface Entry {
+  entryID: number;
+  title: string;
+  imgUrl: string;
+  notes: string;
+}
 const data = {
   view: 'entry-form',
   entries: [] as Entry[],
@@ -8,19 +14,23 @@ const data = {
 // key for localStorage
 const localStorageKey = 'code-journal-data';
 
+// serialize and save to localStorage
 function writeData(data: any): void {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem(localStorageKey, dataJSON);
 }
 
+// load data from localStorage
 function loadData(): void {
   const dataJSON = localStorage.getItem(localStorageKey);
   if (dataJSON) {
     return JSON.parse(dataJSON);
   } else {
+    // if no data - initialize default data
     data.entries = [];
     data.nextEntryId = 1;
   }
 }
 
+// load the data
 loadData();
